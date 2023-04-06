b38b7445a6eb30a98e4831af09ee9e912f24967f
Manuel Flores <manuelflores88@hotmail.com>
2023-04-05 20:17:20 -0700

    Interface login

--- hotelanalytics/tailwind.config.js
+++ hotelanalytics/tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/**/**/*.{html,ts}",
+
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'verde':'#00C087',
+        'titulo':'#AC4A00',
+        'fondo':'#F0F3F8'
+      },
+      width:{
+        '500':'400px'
+      },
+      height:{
+        '700':'500px'
+      }
+    },
   },
   plugins: [],
 }
