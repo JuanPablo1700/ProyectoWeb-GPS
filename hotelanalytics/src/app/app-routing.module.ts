a34ac768e3af3cadf8757fa1cf9e0b9452f459cb
Manuel Flores <manuelflores88@hotmail.com>
2023-04-10 16:30:38 -0700

    Interface inicio

--- hotelanalytics/src/app/app-routing.module.ts
+++ hotelanalytics/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/login',pathMatch:'full'},
-  {path:'login',component: LoginComponent}
- ];
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path:'inicio',component:InicioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
