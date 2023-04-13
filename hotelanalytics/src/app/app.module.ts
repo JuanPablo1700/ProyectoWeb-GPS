dc60b385657269886ee40ec7fb94550409312b7b
jupacamposca <jupacamposca@ittepic.edu.mx>
2023-04-13 16:28:35 -0700

    correcciones

--- hotelanalytics/src/app/app.module.ts
+++ hotelanalytics/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AppComponent } from './app.component';
 //Modulos
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from "@angular/forms";
+import { AddTokenInterceptor } from './utils/add-token.interceptor';
+
 //Componentes
 import { LoginComponent } from './components/login/login.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-<<<<<<< HEAD
 import { PrincipaldirectorComponent } from './components/principaldirector/principaldirector.component';
-=======
-import { AddTokenInterceptor } from './utils/add-token.interceptor';
->>>>>>> 0614dd2e5c6b9ae4f0f2e2273f84fbdc69444ea2
 
 @NgModule({
   declarations: [
