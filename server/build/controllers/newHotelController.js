b837dbe63f2245865d22e7ceb3d6bdaa313fa348
Juan Pablo <juanpablo@iMac-de-Juan.local>
2023-04-19 14:52:03 -0700

    nuevos hoteles y usuarios

--- server/build/controllers/newHotelController.js
+++ server/build/controllers/newHotelController.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.newHotelController = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const database_1 = __importDefault(require("../database"));
+const nodemailer_1 = __importDefault(require("nodemailer"));
 class NewHotelController {
     newHotel(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -29,7 +30,7 @@ class NewHotelController {
                 if (!hotelInsertado) {
                     return res.status(401).json({ msg: 'No se pudo insertar el hotel' });
                 }
-                const nuevoHotel = yield database_1.default.query('SELECT * FROM hotel WHERE correo = ?', [correo]);
+                const nuevoHotel = yield database_1.default.query('SELECT id, correo FROM hotel WHERE correo = ?', [correo]);
                 if (nuevoHotel) {
                     //Crear los nuevos usuarios de gerente y recepcionista
                     let gerente = 'gerente-' + nombre.toLowerCase().replace(/\s+/g, "-");
@@ -37,10 +38,44 @@ class NewHotelController {
                     //Crear las contraseñas y aplicar encode con bcrypt
                     let passGerente = yield bcrypt_1.default.hash(gerente, 10);
                     let passRecepcion = yield bcrypt_1.default.hash(recepcion, 10);
-                    //Insertar los usuarios con el fk_id_hotel = id del hotel que acabamos de crear
+                    let fk_id_hotel = nuevoHotel[0][0][0];
+                    console.log(fk_id_hotel);
+                    try {
+                        //Insertar los usuarios con el fk_id_hotel = id del hotel que acabamos de crear
+                        const insertGerente = yield database_1.default.query('INSERT INTO usuario (user, password, activo, tipo_usuario, pass_actualizada, fk_id_hotel) values (?,?,?,?,?,?)', [gerente, passGerente, '1', 'gerente', 0, fk_id_hotel]);
+                        const insertRecepcion = yield database_1.default.query('INSERT INTO usuario (user, password, activo, tipo_usuario, pass_actualizada, fk_id_hotel) values (?,?,?,?,?,?)', [recepcion, passRecepcion, '1', 'recepcionista', 0, fk_id_hotel]);
+                    }
+                    catch (error) {
+                        console.log(error);
+                    }
                     //mandar correo al de los usuarios al correo registrado del hotel
+                    let transporter = nodemailer_1.default.createTransport({
+                        host: 'smtp.gmail.com',
+                        port: 587,
+                        secure: false,
+                        auth: {
+                            user: 'hotelanalyticss@gmail.com',
+                            pass: 'CR141q$MkBvU3e!CQ'
+                        }
+                    });
+                    let mailOptions = {
+                        from: 'Remitente <hotelanalyticss@gmail.com>',
+                        to: 'jcamposcasillas@gmail.com',
+                        subject: 'Usuarios para accesar a HotelAnalytics',
+                        text: `El usuario y la contraseña para el gerente es: ${gerente} , y el usuario y la contraseña para el recepcionista es: ${recepcion}. Recuerde que debera actualizar la contraseña la primera vez que inicie sesión.`
+                    };
+                    transporter.sendMail(mailOptions, function (error, info) {
+                        if (error) {
+                            console.log(error);
+                            res.status(500).json({ msg: 'Error al enviar el correo electrónico' });
+                        }
+                        else {
+                            console.log('Correo electrónico enviado: ' + info.response);
+                            res.send('Correo electrónico enviado correctamente');
+                        }
+                    });
                     //Si no hay errores mandar respuesta de exitosa
-                    return res.json(gerente);
+                    return res.json({ msj: 'Correo enviado correctamente' });
                 }
                 else {
                     return res.status(401).json({ msg: 'Hotel no encontrado' });
--- server/src/controllers/newHotelController.ts
+++ server/src/controllers/newHotelController.ts
@@ -1,58 +1,96 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
 import pool from '../database';
+import nodemailer from 'nodemailer';
 
 class NewHotelController {
 
-    public async newHotel(req: Request, res: Response) {
+  public async newHotel(req: Request, res: Response) {
 
-        const { nombre, direccion, correo, telefono, estrellas, estado } = req.body;
+    const { nombre, direccion, correo, telefono, estrellas, estado } = req.body;
+
+    try {
+      const existe: any = await pool.query('SELECT * FROM hotel WHERE correo = ?', [correo]);
+
+      if (existe[0] != "") {
+        return res.status(401).json({ msg: 'El hotel ya existe en el sistema' });
+      }
+
+      const fechaIngreso = new Date();
+
+      const hotelInsertado = await pool.query('INSERT INTO hotel (nombre, fecha_ingreso, activo, estrellas, direccion, telefono, correo) values (?,?,?,?,?,?,?)',
+        [nombre, fechaIngreso, estado, estrellas, direccion, telefono, correo]);
+
+      if (!hotelInsertado) {
+        return res.status(401).json({ msg: 'No se pudo insertar el hotel' });
+      }
+
+      const nuevoHotel: any = await pool.query('SELECT id, correo FROM hotel WHERE correo = ?', [correo]);
+
+      if (nuevoHotel) {
+        //Crear los nuevos usuarios de gerente y recepcionista
+        let gerente = 'gerente-' + nombre.toLowerCase().replace(/\s+/g, "-");
+        let recepcion = 'recepcion-' + nombre.toLowerCase().replace(/\s+/g, "-");
+
+        //Crear las contraseñas y aplicar encode con bcrypt
+        let passGerente = await bcrypt.hash(gerente, 10);
+        let passRecepcion = await bcrypt.hash(recepcion, 10);
+
+        let fk_id_hotel = nuevoHotel[0][0][0];
+        
+        console.log(fk_id_hotel);
 
         try {
-            const existe:any = await pool.query('SELECT * FROM hotel WHERE correo = ?', [correo]);
-
-            if (existe[0] != "") {
-                return res.status(401).json({ msg: 'El hotel ya existe en el sistema' });
-            }
-            
-            const fechaIngreso = new Date();
-            
-            const hotelInsertado = await pool.query('INSERT INTO hotel (nombre, fecha_ingreso, activo, estrellas, direccion, telefono, correo) values (?,?,?,?,?,?,?)', 
-            [nombre, fechaIngreso, estado, estrellas, direccion, telefono, correo]);
-            
-            if (!hotelInsertado) {
-                return res.status(401).json({ msg: 'No se pudo insertar el hotel' });
-            }
-
-            const nuevoHotel = await pool.query('SELECT * FROM hotel WHERE correo = ?', [correo]);
-
-            if (nuevoHotel) {
-                //Crear los nuevos usuarios de gerente y recepcionista
-                let gerente = 'gerente-' + nombre.toLowerCase().replace(/\s+/g, "-");
-                let recepcion = 'recepcion-' + nombre.toLowerCase().replace(/\s+/g, "-");
-            
-                //Crear las contraseñas y aplicar encode con bcrypt
-                let passGerente = await bcrypt.hash(gerente, 10);
-                let passRecepcion = await bcrypt.hash(recepcion, 10);
-
-                //Insertar los usuarios con el fk_id_hotel = id del hotel que acabamos de crear
-                
-
-                //mandar correo al de los usuarios al correo registrado del hotel
-
-
-                //Si no hay errores mandar respuesta de exitosa
-
-                return res.json(gerente);
-            }else {
-                return res.status(401).json({ msg: 'Hotel no encontrado' });
-            }
-
+          //Insertar los usuarios con el fk_id_hotel = id del hotel que acabamos de crear
+          const insertGerente = await pool.query('INSERT INTO usuario (user, password, activo, tipo_usuario, pass_actualizada, fk_id_hotel) values (?,?,?,?,?,?)',
+            [gerente, passGerente, '1', 'gerente', 0, fk_id_hotel]);
+  
+          const insertRecepcion = await pool.query('INSERT INTO usuario (user, password, activo, tipo_usuario, pass_actualizada, fk_id_hotel) values (?,?,?,?,?,?)',
+            [recepcion, passRecepcion, '1', 'recepcionista', 0, fk_id_hotel]);
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ msg: 'Error del servidor' });
-        } 
+          console.log(error);
+        }
+
+        //mandar correo al de los usuarios al correo registrado del hotel
+        let transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false,
+          auth: {
+            user: 'hotelanalyticss@gmail.com',
+            pass: 'CR141q$MkBvU3e!CQ'
+          }
+        });
+
+        let mailOptions = {
+          from: 'Remitente <hotelanalyticss@gmail.com>',
+          to: 'jcamposcasillas@gmail.com',
+          subject: 'Usuarios para accesar a HotelAnalytics',
+          text: `El usuario y la contraseña para el gerente es: ${gerente} , y el usuario y la contraseña para el recepcionista es: ${recepcion}. Recuerde que debera actualizar la contraseña la primera vez que inicie sesión.`
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+            res.status(500).json({ msg: 'Error al enviar el correo electrónico' });
+          } else {
+            console.log('Correo electrónico enviado: ' + info.response);
+            res.send('Correo electrónico enviado correctamente');
+          }
+        });
+
+        //Si no hay errores mandar respuesta de exitosa
+
+        return res.json({ msj: 'Correo enviado correctamente' });
+      } else {
+        return res.status(401).json({ msg: 'Hotel no encontrado' });
+      }
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: 'Error del servidor' });
     }
+  }
 }
 
 export const newHotelController = new NewHotelController();
