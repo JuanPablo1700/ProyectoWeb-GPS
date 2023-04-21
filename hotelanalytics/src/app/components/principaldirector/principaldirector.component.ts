282c0d65774c0ce20af5b253f0fb22dbeb047d4f
ArletheMora <armomorali@ittepic.edu.mx>
2023-04-20 22:14:01 -0700

    ver los hoteles desde principalDirector

--- hotelanalytics/src/app/components/principaldirector/principaldirector.component.ts
+++ hotelanalytics/src/app/components/principaldirector/principaldirector.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NewHotelService } from 'src/app/services/new-hotel.service';
+import { ErrorService } from 'src/app/services/error.service';
+import { Hotel } from 'src/app/interfaces/hotel';
 
 @Component({
   selector: 'app-principaldirector',
   templateUrl: './principaldirector.component.html',
   styleUrls: ['./principaldirector.component.css']
 })
-export class PrincipaldirectorComponent {
+export class PrincipaldirectorComponent implements OnInit{
+  listHotel: Hotel[] = [];
+  ultimaMod: number = -1;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private _newHotelService: NewHotelService,
+    private _errorService: ErrorService
   ) {}
 
+  ngOnInit(): void {
+    this.getHotels();
+
+
+  }
+
+  getHotels() {
+    this._newHotelService.getHotels().subscribe({
+      next: (data) => {
+        this.listHotel = data;
+      },
+      error: (error: HttpErrorResponse) => {
+        this._errorService.msjError(error);
+      }
+    })
+  }
 
   nuevo(){
     this.router.navigate(['/nuevohotel']);
