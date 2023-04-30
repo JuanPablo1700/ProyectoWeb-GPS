da1e7b8ac118503fc448638815547fbd7d7b5dee
ArletheMora <armomorali@ittepic.edu.mx>
2023-04-30 14:17:26 -0700

    mostrar motivo en  graficasmihotel

--- hotelanalytics/src/app/components/graficasmihotel/graficasmihotel.component.ts
+++ hotelanalytics/src/app/components/graficasmihotel/graficasmihotel.component.ts
@@ -1,44 +1,47 @@
 import { DataService } from 'src/app/services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-graficasmihotel',
   templateUrl: './graficasmihotel.component.html',
   styleUrls: ['./graficasmihotel.component.css'],
 })
-export class GraficasmihotelComponent implements OnInit{
-  constructor(private router: Router, private dataService: DataService) {}
+export class GraficasmihotelComponent implements OnInit {
+  constructor(private router: Router, private toastr: ToastrService, private dataService: DataService) { }
+
   ngOnInit(): void {
-    this.consultar();
+    this.filtroConsulta = -1;
   }
 
-  tipoGraficas: number = 0;
   filtroConsulta: number = 0;
-  fechaInicio: Date = new Date();
-  fechaFin: Date = new Date();
+  fechaInicio: any = "";
+  fechaFin: any = "";
+
+  graficasVisibles = 0;
 
   motivo: any;
-  parametros:any;
+  parametros: any;
 
-   /* motivo = [
-    {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    },
-    {
-      "name": "France",
-      "value": 7200000
-    },
-      {
-      "name": "UK",
-      "value": 6200000
-    }
-  ]; */
+  /* motivo = [
+   {
+     "name": "Germany",
+     "value": 8940000
+   },
+   {
+     "name": "USA",
+     "value": 5000000
+   },
+   {
+     "name": "France",
+     "value": 7200000
+   },
+     {
+     "name": "UK",
+     "value": 6200000
+   }
+ ]; */
 
   //Gráficas
   // motivo: any;
@@ -48,15 +51,46 @@ export class GraficasmihotelComponent implements OnInit{
     return this.motivo;
   }
 
-
   consultar() {
+    const hoy = new Date();
+
+    if (this.filtroConsulta == -1) {
+      this.graficasVisibles = 0;
+      return this.toastr.error('Seleccione un tipo de gráfica', 'Error');
+    }
+    if (this.filtroConsulta == 1) { //Este día
+      this.fechaInicio = hoy;
+      this.fechaFin = hoy;
+    }
+    if (this.filtroConsulta == 2) { //Esta semana
+      this.fechaInicio = new Date(hoy.setDate(hoy.getDate() - hoy.getDay())); //inicio de la semana actual empezando por domingo
+      this.fechaFin = hoy;
+    }
+    if (this.filtroConsulta == 3) { //Este mes
+      this.fechaInicio = new Date(hoy.getFullYear(), hoy.getMonth(), 1); //inicio de mes actual
+      this.fechaFin = hoy;
+    }
+    if (this.filtroConsulta == 4) { //Personalizado
+      if (this.fechaInicio == "" || this.fechaFin == "") {
+        this.graficasVisibles = 0;
+        return this.toastr.error('Seleccione fecha inicio y fecha fin correctamente', 'Error');
+      }
+      if (this.fechaInicio > this.fechaFin) {
+        this.graficasVisibles = 0;
+        return this.toastr.error('Seleccione fecha inicio y fecha fin correctamente', 'Error');
+      }
+    }
+    console.log('inicio: ' + this.fechaInicio);
+    console.log('fin: ' + this.fechaFin);
+
     this.parametros = {
-      "fechaInicio":this.fechaInicio,
-      "fechaFin":this.fechaFin,
+      "fechaInicio": this.fechaInicio,
+      "fechaFin": this.fechaFin,
       "fk_id_hotel": localStorage.getItem('fk_id_hotel')
     }
 
-    this.dataService.getMotivoHotel(this.parametros).subscribe(data=>{
+    this.graficasVisibles = 1;
+    return this.dataService.getMotivoHotel(this.parametros).subscribe(data => {
       this.motivo = data;
     })
   }
@@ -94,6 +128,6 @@ export class GraficasmihotelComponent implements OnInit{
     localStorage.removeItem('tipo_usuario');
     this.router.navigate(['/login']);
   }
-  
+
 }
 
--- server/src/controllers/dataController.ts
+++ server/src/controllers/dataController.ts
@@ -19,10 +19,9 @@ interface DatosHotel {
 class DataController {
 
     public async getMotivoGeneral(req: Request, res: Response) {
-
         const formulario = req.body;
-        let fechaInicio:Date;
-        let fechaFin:Date;
+        let fechaInicio: Date;
+        let fechaFin: Date;
         const hoy = new Date();
         console.log(hoy);
         console.log(formulario.tipoGrafica);
@@ -34,7 +33,7 @@ class DataController {
 
         if (formulario.fechaSelect == "1") { // Este día
             fechaFin = hoy;
-            
+
             hoy.setDate(hoy.getDate() - 1); // restar un día
             fechaInicio = hoy;
 
@@ -42,13 +41,10 @@ class DataController {
             console.log(fechaFin); // mostrar la fecha resultante
         }
         if (formulario.fechaSelect == "2") { // Esta semana
-            
         }
         if (formulario.fechaSelect == "3") { // Este mes
-            
         }
         if (formulario.fechaSelect == "4") { // Personalizado
-            
         }
 
         const datos: any[] = await pool.query('SELECT h.nombre, mv.motivo, COUNT(*) as cantidad FROM registro_huesped as rg LEFT JOIN habitacion_hotel as hh on hh.id = rg.fk_id_habitacion_hotel LEFT JOIN hotel as h on h.id = hh.fk_id_hotel LEFT JOIN motivo_visita as mv on mv.id = rg.fk_id_motivo GROUP BY h.nombre, mv.motivo ORDER BY h.nombre');
@@ -113,17 +109,27 @@ class DataController {
         return res.json(hoteles);
     }
 
-    public async getMotivoHotel(req: Request, res:Response){
+    public async getMotivoHotel(req: Request, res: Response) {
         const form = req.body;
-        let fechaInicio:Date;
-        let fechaFin:Date;
-        const hoy = new Date();
         console.log(form);
-        console.log(hoy);
+        const fechaInicio = form.fechaInicio;
+        const fechaFin = form.fechaFin;
+        const idHotel = form.fk_id_hotel;
 
-       // const datos: any[] = await pool.query("SELECT h.nombre, mv.motivo, COUNT(*) AS cantidad FROM registro_huesped AS rg LEFT JOIN habitacion_hotel AS hh ON hh.id = rg.fk_id_habitacion_hotel LEFT JOIN hotel AS h ON h.id = hh.fk_id_hotel LEFT JOIN motivo_visita AS mv ON mv.id = rg.fk_id_motivo WHERE h.id = 36 AND (fecha_registro >= '2023-04-21' AND fecha_registro <= '2023-04-26' ) GROUP BY h.nombre, mv.motivo;")
-       const datos: any[] = await pool.query('SELECT h.nombre, mv.motivo, COUNT(*) as cantidad FROM registro_huesped as rg LEFT JOIN habitacion_hotel as hh on hh.id = rg.fk_id_habitacion_hotel LEFT JOIN hotel as h on h.id = hh.fk_id_hotel LEFT JOIN motivo_visita as mv on mv.id = rg.fk_id_motivo GROUP BY h.nombre, mv.motivo ORDER BY h.nombre'); 
-       console.log(datos[0]);
+        const datos: any[] = await pool.query("SELECT h.nombre, mv.motivo, COUNT(*) AS cantidad FROM registro_huesped AS rg LEFT JOIN habitacion_hotel AS hh ON hh.id = rg.fk_id_habitacion_hotel LEFT JOIN hotel AS h ON h.id = hh.fk_id_hotel LEFT JOIN motivo_visita AS mv ON mv.id = rg.fk_id_motivo WHERE h.id = " + idHotel + " AND ( rg.fecha_ingreso >= '" + fechaInicio + "' AND fecha_salida <= '" + fechaFin + "') GROUP BY h.nombre, mv.motivo");
+        //const datos: any[] = await pool.query('SELECT h.nombre, mv.motivo, COUNT(*) as cantidad FROM registro_huesped as rg LEFT JOIN habitacion_hotel as hh on hh.id = rg.fk_id_habitacion_hotel LEFT JOIN hotel as h on h.id = hh.fk_id_hotel LEFT JOIN motivo_visita as mv on mv.id = rg.fk_id_motivo GROUP BY h.nombre, mv.motivo ORDER BY h.nombre');
+        console.log(datos[0]);
+        const motivo = [];
+
+        for (const dato of datos[0]) {
+            motivo.push({
+                name: dato.motivo,
+                value: dato.cantidad
+            });
+        }
+
+        console.log(motivo);
+        return res.json(motivo);
         /* const hoteles: DatosHotel[] = [];
         const mapaHoteles = new Map<string, DatosHotel>();
 
